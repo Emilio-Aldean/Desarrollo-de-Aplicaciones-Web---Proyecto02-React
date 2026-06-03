263d7c731ff7da0b419c4bc192961b27b2f2ac28
Emilio-Aldean <emilio.aldean@gmail.com>
2026-06-03 16:31:30 -0500

    Correcciones en el gráfico de los países

--- src/components/charts/TopCountriesChart.tsx
+++ src/components/charts/TopCountriesChart.tsx
@@ -8,9 +8,19 @@ interface Props {
 }
 
 export default function TopCountriesChart({ data }: Props) {
-  const sorted = [...data].sort((a, b) => b.happinessScore - a.happinessScore).slice(0, 10);
-  const countries = sorted.map((d) => d.country);
-  const scores = sorted.map((d) => Number(d.happinessScore.toFixed(2)));
+  const countryMap = new Map<string, number[]>();
+  data.forEach((d) => {
+    if (!countryMap.has(d.country)) countryMap.set(d.country, []);
+    countryMap.get(d.country)!.push(d.happinessScore);
+  });
+
+  const top10 = [...countryMap.entries()]
+    .map(([country, s]) => ({ country, avg: s.reduce((a, b) => a + b, 0) / s.length }))
+    .sort((a, b) => b.avg - a.avg)
+    .slice(0, 10);
+
+  const countries = top10.map((d) => d.country);
+  const scores = top10.map((d) => Number(d.avg.toFixed(2)));
 
   return (
     <Box>
@@ -19,7 +29,7 @@ export default function TopCountriesChart({ data }: Props) {
       </Typography>
       <BarChart
         height={300}
-        series={[{ data: scores, label: 'Happiness Score', color: '#1e3a5f' }]}
+        series={[{ data: scores, label: 'Happiness Score', color: '#00838f' }]}
         xAxis={[{ scaleType: 'band', data: countries, tickLabelStyle: { fontSize: 10 } }]}
         margin={{ left: 40, right: 10, top: 20, bottom: 60 }}
       />
